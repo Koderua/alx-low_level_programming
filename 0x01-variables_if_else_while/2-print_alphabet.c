85ae582ed8fd37aadd9600132c22f5909c349148
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-04 21:01:58 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function prints out the alphabet in lowercase
+* Return: returns 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar(\n);
+	return (0);
+}
