45d2acfb9072f3f47797e78c6fe589623e3fd279
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-09 22:56:16 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,27 +2,15 @@
 
 int main(void)
 {
-char c1 = 'H';
-char c2 = 'o';
-char c3 = 'l';
-char c4 = 'b';
-char c5 = 'e';
-char c6 = 'r';
-char c7 = 't';
-char c8 = 'o';
-char c9 = 'n';
-char c10 = '\n';
-
-_putchar(c1);
-_putchar(c2);
-_putchar(c3);
-_putchar(c4);
-_putchar(c5);
-_putchar(c6);
-_putchar(c7);
-_putchar(c8);
-_putchar(c9);
-_putchar(c10);
-
-return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
