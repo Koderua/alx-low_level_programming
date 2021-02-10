62b169c661cfc5b8efacd734147b46909c6b2a82
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-10 22:47:04 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* add - adds two integers
+* @n: parameter
+* @m: parameter
+* Return: returns sum
+*/
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
