aa1d49cbb1b001817eb9cce0b676540f833003ff
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-10 05:38:04 +0300

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,62 +1,12 @@
-/**
-*Prototype for
-*_putchar
-*0-holberton.c
-*/
-
 int _putchar(char c);
-
-/**
-*print abc
-*/
 void print_alphabet(void);
-
-/**
-*print 10xabc
-*/
 void print_alphabet_x10(void);
-
-/**
-*check if lower
-*/
 printint _islower(int c);
-
-/**
-*check if alpha
-*/
 int _isalpha(int c);
-
-/**
-*print no. sign
-*/
 int print_sign(int n);
-
-/**
-*int abs value
-*/
 int _abs(int);
-
-/**
-*last digit of no.
-*/
 int print_last_digit(int);
-
-/**
-*jack bauer day
-*/
 void jack_bauer(void);
-
-/**
-*9 times table
-*/
 void times_table(void);
-
-/**
-*add two ints
-*/
 int add(int, int);
-
-/**
-*natural numbers to 98
-*/
 void print_to_98(int n);
