27bb18e7f56b0848f8e9e77b828edfed0f94f10f
Koderua <kibirtbayou@yahoo.com>
2021-02-10 14:04:38 +0000

    fixed errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,18 @@
 
 /**
 * print_alphabet_x10 - prints abc
-* Return - returns sth
+* Return: returns sth
 */
 void print_alphabet_x10(void)
 {
-    int i;
-    char j;
-    for (i = 0; i < 10; i++)
-    {
-    for (j = 'a'; j <= 'z'; j++)
-    {
-      _putchar(j);
-    }
-  _putchar(‘\n’);
+int i;
+char j;
+for (i = 0; i < 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
 }
