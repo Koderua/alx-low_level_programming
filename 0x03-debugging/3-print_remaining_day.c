383df5ddb20b7b41278f55ed8af3fd492c5c455a
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-10 23:10:52 +0300

    Update 3-print_remaining_day.c

--- 0x03-debugging/3-print_remaining_day.c
+++ 0x03-debugging/3-print_remaining_day.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are left
-* in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
+* print_remaining_days - prints days left
+* @month: parameter
+* @day: parameter
+* @year: parameter
+* Return: returns
 */
 
 void print_remaining_days(int month, int day, int year)
