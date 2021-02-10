d744cd4bf042cd937821d9a1c23ffbae7be74d1a
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-10 22:48:28 +0300

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* print_to_98 - prints natural numbers to 98
+* @n: parameter
+* Return: returns
+*/
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n++;
+		}
+	}
+}
