d35cedb55605aa27beffbfda1b9c2e736e67ded5
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-10 06:43:12 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,10 @@ void print_alphabet_x10(void)
 int i = 0;
 for (i = 0; i < 10; i++)
 {
-_putchar([‘a’ ‘z’]);
-}
-_putchar(‘\n’);
-return (0);
+    for (j = 'a'; j <= 'z'; j++)
+    {
+      _putchar(j);
+    }
+  _putchar(‘\n’);
+  return (0);
 }
