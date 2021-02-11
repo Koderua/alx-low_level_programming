d10f6dfc86b5d418118ed85d7e1cafa23e5a4598
Koderua <kibirtbayou@yahoo.com>
2021-02-11 14:11:21 +0000

    created file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper - checks ABC
+ * Return: returns 1 or 0
+ */
+
+int _isupper(int c)
+{
+if(c >= 'A' and c <= 'Z')
+    {
+      return (1);
+    }
+ else
+   {
+     return (0);
+   }
+}
