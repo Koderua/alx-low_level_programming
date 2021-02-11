fe8724417ca73b499a562f2cbe56bd02876c14fb
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-11 22:55:16 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _isupper - checks ABC
- * @c: parameter
- * Return: returns 0 or 1
- */
+* _isupper - Checks ABC
+* @c: parameter
+* Return: 1 or 0
+*/
 int _isupper(int c)
 {
-if (c >= 'A' and c <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
