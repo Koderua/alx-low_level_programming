5bf130734464df5ed56576d354953f8a2b2e178c
Koderua <kibirtbayou@yahoo.com>
2021-02-11 19:52:17 +0000

    fixed errors

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - checks ABC
- * @c - parameter
- * Return: returns 1 or 0
+ * @c: parameter
+ * Return: returns 0 or 1
  */
 int _isupper(int c)
 {
-    if (c >= 'A' and c <= 'Z')
-        retuen (1);
-    else
-        return (0);
+if (c >= 'A' and c <= 'Z')
+return (1);
+else
+return (0);
 }
