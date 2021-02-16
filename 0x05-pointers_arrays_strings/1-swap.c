f8b86bb715332187206141996d4111fe0b5002bf
Koderua <kibirtbayou@yahoo.com>
2021-02-16 20:48:51 +0000

    fingers crossed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *swap_int - swaps
+ *@p: par
+ *@q: par
+ *Return: void
+ */
+void swap_int(int *p, int *q)
+{
+*p = b;
+*q = a;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,9 @@
 /**
  *reset_to_98
- *
 */
 void reset_to_98(int *n);
+
+/**
+ *swap_int
+ */
+void swap_int(int *a, int *b);
