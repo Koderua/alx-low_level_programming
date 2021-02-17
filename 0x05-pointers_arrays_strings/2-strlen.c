86721b6189329014eefb0250fede6ada0c8f5201
Koderua <kibirtbayou@yahoo.com>
2021-02-17 12:02:16 +0000

    sweet agony

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_strlen - returns strlen
+*@s: par
+*Return: int
+*/
+int _strlen(char *s)
+{
+int i;
+int c;
+for (i = 0; s[i] != '\0'; i++)
+{
+return (i);
+}
+c = i;
+return (c);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,3 +7,8 @@ void reset_to_98(int *n);
  *swap_int
  */
 void swap_int(int *a, int *b);
+
+/**
+ *_strlen
+ */
+int _strlen(char *s);
