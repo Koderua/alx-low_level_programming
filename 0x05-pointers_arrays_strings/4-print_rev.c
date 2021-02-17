0e72df5c96c792a21009c035fc015e926d40cccb
Koderua <69991794+Koderua@users.noreply.github.com>
2021-02-17 23:03:20 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_rev - prints
+* @s: par
+* Return: no return
+*/
+void print_rev(char *s)
+{
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (i = c - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
