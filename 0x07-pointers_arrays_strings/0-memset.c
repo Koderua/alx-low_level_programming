c9de55aef8e343d3bc6e73e8762a446fcf3518b9
Koderua <kibirtbayou@yahoo.com>
2021-02-23 04:59:23 +0000

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+/**
+ *_memset - fn
+ *@s: p1
+ *@b: p2
+ *@n: p3
+ *Return: s
+ */
+_memset(char *s, char b, unsigned int n)
+{
+char *s;
+const char b;
+int i;
+int n;
+s = &n
+for (s = 0; s < (n + 1); s++)
+{
+*s = b;
+}
+return (s);
+}
