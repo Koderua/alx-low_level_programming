ad06b1cef90a3ea640e5db346018493d1681fccc
Koderua <69991794+Koderua@users.noreply.github.com>
2021-03-09 19:45:24 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #!/bin/bash
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
 return(0);
 }
