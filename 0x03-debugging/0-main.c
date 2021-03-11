6095c8061b5dc1e1199a21136812fa1c0323a40c
Koderua <kibirtbayou@yahoo.com>
2021-03-11 14:27:34 +0000

    created file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,32 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *positive_or_negative - function
+ *@n: parameter
+ * Return: to main
+ */
+int positive_or_negative(int n)
+{
+if (n > 0)
+printf("is positive\n");
+else if (n == 0)
+printf("is zero\n");
+else
+printf("is negative\n");
+return(0);
+}
 
 /**
-* main - tests function
-* Return: returns 0
-*/
-
+ *main - function
+ *Return: 0
+ */
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
+i = 0;
+positive_or_negative(i);
 
-	positive_or_negative(i);
-	return (0);
+return (0);
 }
