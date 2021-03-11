d523a8b64a21a9f312fc570da1fd06baf8529ab8
Koderua <69991794+Koderua@users.noreply.github.com>
2021-03-11 17:55:41 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,4 @@
 #include <holberton.h>
-/**
- *positive_or_negative - function
- *@n: parameter
- * Return: to main
- */
-int positive_or_negative(int n)
-{
-if (n > 0)
-printf("is positive\n");
-else if (n == 0)
-printf("is zero\n");
-else
-printf("is negative\n");
-return(0);
-}
 
 /**
  *main - function
