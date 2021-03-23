60ce3f60b1ebcb7fb4e3fd487fc09f221b9552dc
KibiBC <69991794+Koderua@users.noreply.github.com>
2021-03-23 12:37:06 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,24 +1,30 @@
 #include "lists.h"
-
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * @head: pointer to head of list
- * @index: index of value to be returned
- * Return: address of node at input index
- */
+  *get_nodeint_at_index - function that return an index of node
+  *@head: the head of the linked list
+  *@index: the index we want to remove
+  *Return: the node from the index
+  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current;
+unsigned int i = 0;
+listint_t *tm;
 
-	register uint count = 0;
-
-	current = head;
-	while (current)
-	{
-		if (count == index)
-			return (current);
-		count++;
-		current = current->next;
-	}
-	return (current);
+if (!head)
+return (0);
+else
+{
+if (index == 0)
+return (head);
+i++;
+tm = head->next;
+while (tm)
+{
+if (index == i)
+return (tm);
+tm = tm->next;
+i++;
+}
+}
+return (NULL);
 }
